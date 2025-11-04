8bc5ff7dfd223c745f83f1b8d57fbbef518f2186
Leandro Boxler <leandroboxler@gmail.com>
2025-11-04 11:50:39 -0300

    Add MockCard class to create ICard test objects

---
+++ MiJuego.Tests/Mocks/MockCard.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using MiJuego.Domain.Interfaces;
+using Moq;
+
+namespace MiJuego.Tests.Mocks;
+
+public static class MockCard
+{
+    private static readonly Random Rand = new();
+    private static string Name => $"Card {Rand.Next(1000, 9999)}";
+
+    public static Mock<ICard> CreateMockCardRandom()
+    {
+        var mock = new Mock<ICard>();
+        var random = new Random();
+        var typeCards = Enum.GetValues(typeof(CardType));
+
+        mock.SetupAllProperties(); 
+
+        mock.Object.Name = $"Card {Rand.Next(1000, 9999)}";
+        mock.Object.Description = Rand.Next(1000, 9999).ToString();
+        mock.Object.Type = (CardType)typeCards.GetValue(random.Next(typeCards.Length))!;
+        mock.Object.Value = Rand.Next(5, 15);
+        
+            
+        return mock;
+    }
+}
